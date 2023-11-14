247c9c6db768cf17e9640badb6ddeea50adab11f
Manikanta Pindi <131873708+manikanta-pindi@users.noreply.github.com>
2023-11-14 18:50:49 +0530

    cors remove

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ const client = new ApolloClient({
   // uri: "http://localhost:4001/graphql",
   uri: `https://content.tinajs.io/v1.22/content/0ad2b629-c4a9-42c8-9e42-b8816dbbecd0/github/main`,
   cache: new InMemoryCache(),
+  mode: 'no-cors',
 });
 
 export default function App({ Component, pageProps }) {
