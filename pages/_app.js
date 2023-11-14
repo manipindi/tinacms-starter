d8d6e0f5c6d438c45c8752dd17a9569b4ce1ac92
Manikanta Pindi <131873708+manikanta-pindi@users.noreply.github.com>
2023-11-14 18:33:01 +0530

    added uri for grapghql

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import "@/styles/globals.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4001/graphql",
+  // uri: "http://localhost:4001/graphql",
+  uri: `https://content.tinajs.io/1.22/content/0ad2b629-c4a9-42c8-9e42-b8816dbbecd0/github/main`,
   cache: new InMemoryCache(),
 });
 
