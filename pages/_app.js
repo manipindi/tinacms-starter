88db117a2be98bb9d1575adf9aa3728fcdbdfcbe
Manikanta Pindi <131873708+manikanta-pindi@users.noreply.github.com>
2023-11-14 18:39:24 +0530

    version

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   // uri: "http://localhost:4001/graphql",
-  uri: `https://content.tinajs.io/1.22/content/0ad2b629-c4a9-42c8-9e42-b8816dbbecd0/github/main`,
+  uri: `https://content.tinajs.io/v1.22/content/0ad2b629-c4a9-42c8-9e42-b8816dbbecd0/github/main`,
   cache: new InMemoryCache(),
 });
 
