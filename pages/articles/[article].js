f0c7a3789c24a2931fc286f396efb2f845972972
Manikanta Pindi <131873708+manikanta-pindi@users.noreply.github.com>
2024-02-19 14:50:09 +0530

    removed getstaticpaths

--- pages/articles/[article].js
+++ pages/articles/[article].js
@@ -4,14 +4,15 @@ export default function Article({ username }) {
   return <div>{username}</div>;
 }
 
-export const getStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: "blocking",
-  };
-};
+// export const getStaticPaths = async () => {
+//   return {
+//     paths: [],
+//     fallback: "blocking",
+//   };
+// };
 
 export const getServerSideProps = async ({ params }) => {
+    console.log(params, "params");
   const article = params?.article;
 
   return {
